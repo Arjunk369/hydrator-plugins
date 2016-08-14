689d25bfdc7189e1cf4e38675b286c9a11617929
Yue Gao <Yue@Casks-MacBook-3.local>
2016-08-13 22:42:58 -0700

    add output schema

--- core-plugins/src/main/java/co/cask/hydrator/plugin/batch/source/BigQuerySource.java
+++ core-plugins/src/main/java/co/cask/hydrator/plugin/batch/source/BigQuerySource.java
@@ -26,6 +26,8 @@ import co.cask.cdap.api.dataset.lib.KeyValue;
 import co.cask.cdap.api.plugin.Plugin;
 import co.cask.cdap.api.plugin.PluginConfig;
 import co.cask.cdap.etl.api.Emitter;
+import co.cask.cdap.etl.api.PipelineConfigurer;
+import co.cask.cdap.etl.api.batch.BatchRuntimeContext;
 import co.cask.cdap.etl.api.batch.BatchSourceContext;
 import co.cask.hydrator.common.Constants;
 import co.cask.hydrator.common.ReferenceBatchSource;
@@ -34,7 +36,9 @@ import co.cask.hydrator.common.SourceInputFormatProvider;
 import com.google.cloud.hadoop.io.bigquery.BigQueryConfiguration;
 import com.google.cloud.hadoop.io.bigquery.GsonBigQueryInputFormat;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.gson.JsonObject;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -45,6 +49,11 @@ import org.slf4j.LoggerFactory;
 import scala.util.parsing.json.JSONFormat;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Nullable;
 
 /**
  * Batch source to read from a BigQuery table
@@ -56,14 +65,46 @@ import java.io.IOException;
 public class BigQuerySource extends ReferenceBatchSource<LongWritable, JsonObject, StructuredRecord> {
   private static final Logger LOG = LoggerFactory.getLogger(BigQuerySource.class);
   private final BQSourceConfig sourceConfig;
+  private static final Schema DEFAULT_SCHEMA = Schema.recordOf(
+    "event",
+    Schema.Field.of("title", Schema.of(Schema.Type.STRING)),
+    Schema.Field.of("unique_words", Schema.of(Schema.Type.INT))
+  );
+
+  private Map<String, String> outputSchemaMapping = new HashMap<>();
+  private Schema outputSchema;
 
   public BigQuerySource(BQSourceConfig config) {
     super(new ReferencePluginConfig(config.referenceName));
     this.sourceConfig = config;
   }
+
+  @Override
+  public void initialize(BatchRuntimeContext context) throws Exception {
+    super.initialize(context);
+    init();
+  }
+
+  private void init() {
+    if (!Strings.isNullOrEmpty(sourceConfig.outputSchema)) {
+      String[] schemaList = sourceConfig.outputSchema.split(",");
+      for (String schema : schemaList) {
+        String[] columns = schema.split(":");
+        outputSchemaMapping.put(columns[0], columns[1]);
+      }
+    }
+  }
+
+  @Override
+  public void configurePipeline(PipelineConfigurer pipelineConfigurer) {
+    super.configurePipeline(pipelineConfigurer);
+    init();
+    getOutputSchema();
+    pipelineConfigurer.getStageConfigurer().setOutputSchema(outputSchema);
+  }
+
   @Override
   public void prepareRun(BatchSourceContext context) throws IOException {
-    String jobname = "BigQueryJob";
     Job job = Job.getInstance();
     Configuration conf = job.getConfiguration();
     conf.set(BigQueryConfiguration.PROJECT_ID_KEY, sourceConfig.projectId);
@@ -96,32 +137,22 @@ public class BigQuerySource extends ReferenceBatchSource<LongWritable, JsonObjec
 
   @Override
   public void transform(KeyValue<LongWritable, JsonObject> input, Emitter<StructuredRecord> emitter) {
+    getOutputSchema();
     LOG.debug("input is {}", input.getValue());
     emitter.emit(jsonTransform(input.getValue()));
   }
 
   private StructuredRecord jsonTransform(JsonObject jsonObject) {
-    Schema innerSchema = Schema.recordOf(
-      "inner",
-      Schema.Field.of("innerInt", Schema.of(Schema.Type.INT)),
-      Schema.Field.of("innerString", Schema.of(Schema.Type.STRING)));
-    Schema schema = Schema.recordOf(
-      "event",
-      Schema.Field.of("intField", Schema.of(Schema.Type.INT)),
-      Schema.Field.of("recordField", innerSchema));
-
-    StructuredRecord record = StructuredRecord.builder(schema)
-      .set("intField", 5)
-      .set("recordField",
-           StructuredRecord.builder(innerSchema)
-             .set("innerInt", 7)
-             .set("innerString", "hello world")
-             .build()
-      )
-      .build();
-
-    return record;
+    StructuredRecord.Builder builder = StructuredRecord.builder(outputSchema);
+    for (Schema.Field field : outputSchema.getFields()) {
+      String fieldName = field.getName();
+      builder.set(fieldName, jsonObject.get(fieldName));
+    }
+    return builder.build();
   }
+  /**
+   * {@link PluginConfig} class for {@link BigQuerySource}
+   */
   public static class BQSourceConfig extends PluginConfig {
     public static final String IMPORT_QUERY = "importQuery";
     public static final String PROJECT_ID   = "projectId";
@@ -140,10 +171,38 @@ public class BigQuerySource extends ReferenceBatchSource<LongWritable, JsonObjec
     @Macro
     String projectId;
 
+    @Name("outputSchema")
+    @Description("Comma separated mapping of column names in the output schema to the data types;" +
+      "for example: 'A:string,B:int'. This input is required if no inputs " +
+      "for 'columnList' has been provided.")
+    @Macro
+    private String outputSchema;
+
     @Name(INPUT_TABLE_ID)
     @Description("The BigQuery table to read from, in the form [optional projectId]:[datasetId].[tableId]." +
                  " Example: publicdata:samples.shakespeare")
     @Macro
     String fullyQualifiedInputTableId;
   }
+
+  private void getOutputSchema() {
+    if (outputSchema == null) {
+      List<Schema.Field> outputFields = Lists.newArrayList();
+      try {
+        // If processing of all the columns are required
+          for (String fieldName : outputSchemaMapping.keySet()) {
+            String columnName = fieldName;
+            Schema fieldType = Schema.of(Schema.Type.valueOf(outputSchemaMapping.get(fieldName).toUpperCase()));
+            outputFields.add(Schema.Field.of(columnName, fieldType));
+          }
+
+      } catch (Exception e) {
+        throw new IllegalArgumentException("Exception while creating output schema " +
+                                             "Invalid output " + "schema: " + e.getMessage(), e);
+      }
+      outputSchema = Schema.recordOf("outputSchema", outputFields);
+    }
+  }
+
+
 }
